0cb56c592b33e374f35324e44ac542d788a90852
jessicaduarte95 <jessicaduarte.95@gmail.com>
2023-05-29 10:32:51 -0300

    Ajuste Responsividade

--- src/components/Currency-style.tsx
+++ src/components/Currency-style.tsx
@@ -1,27 +1,31 @@
 import styled from "styled-components";
 
 export const CurrencyStyled = styled.div `
-width: 50%;
-padding: 50px;
+    width: 50%;
+    padding: 50px;
 
-@media screen and (max-width: 600px){
-    width: 300%;
-}
+    @media screen and (max-width: 600px){
+        width: 300%;
+    }
 `
 
 export const NameDataStyled = styled.div `
     display: flex;
     height: 28%;
+
+    @media screen and (max-width: 600px){
+        flex-direction: column;
+    }
 `
 
 export const NameStyled = styled.div `
-font: bolder 5rem "Fira Sans", serif;
---tw-text-opacity: 1;
-color: rgba(0,168,104,var(--tw-text-opacity));
+    font: bolder 5rem "Fira Sans", serif;
+    --tw-text-opacity: 1;
+    color: rgba(0,168,104,var(--tw-text-opacity));
 
-@media screen and (min-width: 300px) and (max-width: 1400px){
-    font: bolder 3.5rem "Fira Sans", serif;
-}
+    @media screen and (max-width: 1400px){
+        font: bolder 3.5rem "Fira Sans", serif;
+    }
 `
 
 export const LineNameStyled = styled.div `
@@ -53,13 +57,17 @@ export const LineNameStyled = styled.div `
 `
 
 export const DataStyled = styled.div `
- font-size: 1rem;
- padding-top: 2.5rem;
- font-family: 'roboto';
- margin-Left: 2rem;
+    font-size: 1rem;
+    padding-top: 2.5rem;
+    font-family: 'roboto';
+    margin-Left: 2rem;
 
- @media screen and (min-width: 300px) and (max-width: 1400px){
-    font-size: 0.9rem;
-    padding-top: 1.6rem;
-}
+    @media screen and (max-width: 1400px){
+        font-size: 0.9rem;
+        padding-top: 1.6rem;
+    }
+
+    @media screen and (max-width: 600px){
+        margin-Left: 0;
+    }
 `
