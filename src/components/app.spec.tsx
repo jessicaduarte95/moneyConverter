9354ff2952318a34ddb8f8a3c7fcbd03acf191e0
jessicaduarte95 <jessicaduarte.95@gmail.com>
2023-06-07 16:00:07 -0300

    Ajuste no test

--- src/components/app.spec.tsx
+++ src/components/app.spec.tsx
@@ -2,8 +2,10 @@ import { render } from '@testing-library/react'
 import { App } from '../App';
 
 describe('App Component', () => {
-    it('Should return converter', () => {
+    it('Should return some text.', () => {
         const { getByText } = render(<App/>);
+        expect(getByText('converter'));
         expect(getByText('currency'));
+        expect(getByText('Dados disponibilizados pela Morningstar.'));
     })
 })
