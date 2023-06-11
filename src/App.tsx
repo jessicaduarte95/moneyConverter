213cf696d289ee142b574057b96a044db4cf823c
jessicaduarte95 <jessicaduarte.95@gmail.com>
2023-06-11 11:22:44 -0300

    Pegando o conteúdo do input

--- src/App.tsx
+++ src/App.tsx
@@ -14,18 +14,28 @@ import {
   CheckboxStyeld } 
   from './components/Currency-style';
   import { VscArrowSwap } from 'react-icons/vsc';
-  import UseRadioGroup from './components/checkbox'
+  import UseRadioGroup from './components/checkbox';
+  import { useForm, SubmitHandler } from "react-hook-form";
+
+  type Input = {
+    money: number,
+  };
 
 export const  App = () =>  {
 
   const date = new Date()
-
   const day = date.getDate();
   const month = date.toLocaleString('default', { month: 'long' });
   const monthOrganized = month[0].toUpperCase() + month.substring(1);
   const currentYear = date.getFullYear();
   const hours = date.getHours() ;
   const minutes = date.getMinutes();
+  const { register, handleSubmit, reset } = useForm<Input>();
+
+  const onSubmit: SubmitHandler<Input> = (data) => {
+    console.log('Teste', data)
+    reset()
+  }
 
   return (
     <ContainerStyled>
@@ -46,22 +56,24 @@ export const  App = () =>  {
             <p>Dados disponibilizados pela Morningstar.</p>
           </DataStyled>
         </NameDataStyled>
-        <ContainerConverter>
-          <MoneyConverter>
-            <p>Dólar:</p>
-            <input type="number" name='Money' placeholder='$0.00'/>
-          </MoneyConverter>
-          <TypeMoney>
-            <p>Tipo de Compra</p>
-            <CheckboxStyeld>
-              <UseRadioGroup/>
-            </CheckboxStyeld>
-          </TypeMoney>
-          <ButtonConverter>
-            <VscArrowSwap style={{color: "white"}}/>
-            <p>Converter</p>
-          </ButtonConverter>
-        </ContainerConverter>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <ContainerConverter>
+            <MoneyConverter>
+              <p>Dólar:</p>
+              <input type="number" placeholder='$0.00' {...register("money")}/>
+            </MoneyConverter>
+            <TypeMoney>
+              <p>Tipo de Compra</p>
+              <CheckboxStyeld>
+                <UseRadioGroup/>
+              </CheckboxStyeld>
+            </TypeMoney>
+            <ButtonConverter type="submit">
+              <VscArrowSwap style={{color: "white"}}/>
+              <p>Converter</p>
+            </ButtonConverter>
+          </ContainerConverter>
+        </form>
       </CurrencyStyled>
       <PhotoStyled>
         <img src="../public/money.webp" alt="Imagem de dinheiro" />
