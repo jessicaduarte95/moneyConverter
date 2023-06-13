6c01b4b01baf1812fdae80dff1aa6720e7f39853
jessicaduarte95 <jessicaduarte.95@gmail.com>
2023-06-13 15:15:47 -0300

    Ajuste do checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,43 @@ import {
   CheckboxStyeld } 
   from './components/Currency-style';
   import { VscArrowSwap } from 'react-icons/vsc';
-  import UseRadioGroup from './components/checkbox';
   import { useForm, SubmitHandler } from "react-hook-form";
+  import { styled } from '@mui/material/styles';
+  import RadioGroup, { useRadioGroup } from '@mui/material/RadioGroup';
+  import FormControlLabel, { FormControlLabelProps } from '@mui/material/FormControlLabel';
+  import Radio from '@mui/material/Radio';
+
+  const StyledFormControlLabel = styled((props: StyledFormControlLabelProps) => (
+    <FormControlLabel {...props} />
+  ))(({ checked }) => ({
+    '.MuiFormControlLabel-label': checked 
+  }));
+  
+  function MyFormControlLabel(props: FormControlLabelProps) {
+    const radioGroup = useRadioGroup();
+  
+    let checked = false;
+  
+    if (radioGroup) {
+      checked = radioGroup.value === props.value;
+    }
+  
+    return <StyledFormControlLabel checked={checked} {...props} />;
+  }
 
   type Input = {
-    money: number,
+    dinheiro: string,
+    cartao: string,
   };
+
+  interface IFormInput {
+    dolar: number,
+    typePay: Input;
+  }
+
+  interface StyledFormControlLabelProps extends FormControlLabelProps {
+    checked: boolean;
+  }
 
 export const  App = () =>  {
 
@@ -30,9 +61,9 @@ export const  App = () =>  {
   const currentYear = date.getFullYear();
   const hours = date.getHours() ;
   const minutes = date.getMinutes();
-  const { register, handleSubmit, reset } = useForm<Input>();
+  const { register, handleSubmit, reset, getValues } = useForm<IFormInput>();
 
-  const onSubmit: SubmitHandler<Input> = (data) => {
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
     console.log('Teste', data)
     reset()
   }
@@ -60,12 +91,15 @@ export const  App = () =>  {
           <ContainerConverter>
             <MoneyConverter>
               <p>Dólar:</p>
-              <input type="number" placeholder='$0.00' {...register("money")}/>
+              <input type="number" placeholder='$0.00' {...register("dolar")}/>
             </MoneyConverter>
             <TypeMoney>
               <p>Tipo de Compra</p>
               <CheckboxStyeld>
-                <UseRadioGroup/>
+                <RadioGroup {...register("typePay")} name="use-radio-group" defaultValue="dinheiro" style={{display: 'flex', flexDirection: 'row'}}>
+                  <MyFormControlLabel value="dinheiro" label="Dinheiro" control={<Radio style={{color: 'green'}} />} />
+                  <MyFormControlLabel value="cartao" label="Cartão" control={<Radio style={{color: 'green'}}/>} />
+                </RadioGroup>
               </CheckboxStyeld>
             </TypeMoney>
             <ButtonConverter type="submit">
